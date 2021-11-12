a40175d9120d7cdb8ff64f89117015beccea34d1
Ursula Kesek <ursky@blueyonder.co.uk>
2021-11-12 12:50:48 +0000

    added build/ and modifiedfiles

---
+++ build/precache-manifest.27c9afcb3e63afdca133f68088b6d4bb.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc73f5afd546478097e81d9fba2d5f61",
+    "url": "/uk-perfumes/index.html"
+  },
+  {
+    "revision": "5aa22a854440642c050e",
+    "url": "/uk-perfumes/static/css/main.2f8a807a.chunk.css"
+  },
+  {
+    "revision": "f875ca4dd2730dbedb60",
+    "url": "/uk-perfumes/static/js/2.5bbb9943.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/uk-perfumes/static/js/2.5bbb9943.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aa22a854440642c050e",
+    "url": "/uk-perfumes/static/js/main.1e001e2d.chunk.js"
+  },
+  {
+    "revision": "063a662fce386686fdad",
+    "url": "/uk-perfumes/static/js/runtime-main.ee7f1a49.js"
+  },
+  {
+    "revision": "ae0063a7f5bbb3dfb19129b9b242e967",
+    "url": "/uk-perfumes/static/media/Bluecat404.ae0063a7.gif"
+  },
+  {
+    "revision": "80366b0861afb45ac71cbd909165890f",
+    "url": "/uk-perfumes/static/media/amyris-oil.80366b08.png"
+  },
+  {
+    "revision": "69bb30a4da0f4a15750447c845a95ee6",
+    "url": "/uk-perfumes/static/media/aromatic.69bb30a4.png"
+  },
+  {
+    "revision": "4f2f3be72e4b53283fe2fa74077a3feb",
+    "url": "/uk-perfumes/static/media/aromatic1.4f2f3be7.png"
+  },
+  {
+    "revision": "c614b6fda053d703bcbf00c0a0562047",
+    "url": "/uk-perfumes/static/media/aw-front.c614b6fd.png"
+  },
+  {
+    "revision": "1147d2a543a83c209e8d4e9ea5a337f0",
+    "url": "/uk-perfumes/static/media/aw-front2.1147d2a5.png"
+  },
+  {
+    "revision": "d8bed72e7e493bbaa439d5bc81b96cc5",
+    "url": "/uk-perfumes/static/media/bluecat.d8bed72e.png"
+  },
+  {
+    "revision": "add639cca481d9abdbf959fb762d6c53",
+    "url": "/uk-perfumes/static/media/bubble-iv.add639cc.gif"
+  },
+  {
+    "revision": "7653fd515c2877d23fdb0bbfe131e799",
+    "url": "/uk-perfumes/static/media/citrus1.7653fd51.png"
+  },
+  {
+    "revision": "fe696863b0bb374d2044eaec13faa08f",
+    "url": "/uk-perfumes/static/media/citrus2.fe696863.png"
+  },
+  {
+    "revision": "94db1c593aef3803000a80bfe6472935",
+    "url": "/uk-perfumes/static/media/clarysage.94db1c59.png"
+  },
+  {
+    "revision": "4d353cd8e192e553e4a734aadcd33631",
+    "url": "/uk-perfumes/static/media/floral2a.4d353cd8.png"
+  },
+  {
+    "revision": "72d62acc682e735aff517c8468a4cfbe",
+    "url": "/uk-perfumes/static/media/floral2b.72d62acc.png"
+  },
+  {
+    "revision": "fefa5b1ff8d0695e07c7bfffc49d5793",
+    "url": "/uk-perfumes/static/media/floral2c.fefa5b1f.png"
+  },
+  {
+    "revision": "77df1521b3fb7debc65234352191d301",
+    "url": "/uk-perfumes/static/media/florala.77df1521.png"
+  },
+  {
+    "revision": "3fd8fe09e9fc9d143a77d449fed35c1a",
+    "url": "/uk-perfumes/static/media/floralb.3fd8fe09.png"
+  },
+  {
+    "revision": "6cf4f01d0a0374e6a7427c1c8d3e244d",
+    "url": "/uk-perfumes/static/media/floralc.6cf4f01d.png"
+  },
+  {
+    "revision": "2e53694e4e8d589ab44aaf409edc96c1",
+    "url": "/uk-perfumes/static/media/fragrance-wheel.2e53694e.png"
+  },
+  {
+    "revision": "5f40d3f8674e183ebcdc73fc3dff5e30",
+    "url": "/uk-perfumes/static/media/frankincense.5f40d3f8.png"
+  },
+  {
+    "revision": "e633595c67e14af5f2f70b9197a060ba",
+    "url": "/uk-perfumes/static/media/fruity1.e633595c.png"
+  },
+  {
+    "revision": "587e4d2391fb4529ddc325f196401d7e",
+    "url": "/uk-perfumes/static/media/fruity2.587e4d23.png"
+  },
+  {
+    "revision": "d72cabeac7145f2e757571f70996cc5f",
+    "url": "/uk-perfumes/static/media/green1.d72cabea.png"
+  },
+  {
+    "revision": "86f478a3a2bd9ad309648a872ef432ed",
+    "url": "/uk-perfumes/static/media/greena.86f478a3.png"
+  },
+  {
+    "revision": "600d4d9063815f859e576f3c6d950d50",
+    "url": "/uk-perfumes/static/media/greenb.600d4d90.png"
+  },
+  {
+    "revision": "7697bca5392aa5bd7c1d7d9e6f008a69",
+    "url": "/uk-perfumes/static/media/heliotrope.7697bca5.png"
+  },
+  {
+    "revision": "da2e47bd91a43828755a6ad730a47d47",
+    "url": "/uk-perfumes/static/media/jasmine.da2e47bd.png"
+  },
+  {
+    "revision": "7b91d50f6b72b4d8ba43cdcf0a3da134",
+    "url": "/uk-perfumes/static/media/lavender.7b91d50f.png"
+  },
+  {
+    "revision": "50ff4f8e8fb3c259eaec0aff497274b7",
+    "url": "/uk-perfumes/static/media/lemongrass.50ff4f8e.png"
+  },
+  {
+    "revision": "53767b7dac9883d85912dc184854e3a8",
+    "url": "/uk-perfumes/static/media/mimosa.53767b7d.png"
+  },
+  {
+    "revision": "56d96709911d6c25d2ade2724c47f568",
+    "url": "/uk-perfumes/static/media/mossy.56d96709.png"
+  },
+  {
+    "revision": "382840ada79cd5222cd224ad1deb1e51",
+    "url": "/uk-perfumes/static/media/mossy1.382840ad.png"
+  },
+  {
+    "revision": "0a415e12a09dc924845e15e01771cb10",
+    "url": "/uk-perfumes/static/media/muguet.0a415e12.png"
+  },
+  {
+    "revision": "bebb491e6fc98620059954f9c270e61d",
+    "url": "/uk-perfumes/static/media/oakmoss.bebb491e.png"
+  },
+  {
+    "revision": "cb86a34fa456ba74979e889db3d617ba",
+    "url": "/uk-perfumes/static/media/oriental.cb86a34f.png"
+  },
+  {
+    "revision": "95279861fc2299add222a3badce019aa",
+    "url": "/uk-perfumes/static/media/oriental1.95279861.png"
+  },
+  {
+    "revision": "b950297dd098f75ef2ec226c627f4422",
+    "url": "/uk-perfumes/static/media/rockrose.b950297d.png"
+  },
+  {
+    "revision": "5ffaa7162d1dfb8e463b4dcfb0a307e4",
+    "url": "/uk-perfumes/static/media/sandalwood.5ffaa716.png"
+  },
+  {
+    "revision": "3f04b3560e9bfd140d5db2d111d6f388",
+    "url": "/uk-perfumes/static/media/spicy.3f04b356.png"
+  },
+  {
+    "revision": "2d6e2f648f66174f12e2b2ab8d9d3e8f",
+    "url": "/uk-perfumes/static/media/spicy1.2d6e2f64.png"
+  },
+  {
+    "revision": "576ccb29e8d18535d97cd17383b144f5",
+    "url": "/uk-perfumes/static/media/verveine.576ccb29.png"
+  },
+  {
+    "revision": "a696589ab1930ae9a9e5b622f02612a1",
+    "url": "/uk-perfumes/static/media/vetiver.a696589a.png"
+  },
+  {
+    "revision": "d12a9fe806526f70b432a06ff80376ac",
+    "url": "/uk-perfumes/static/media/water1.d12a9fe8.png"
+  },
+  {
+    "revision": "602bc0e57f092840611fa8d77dfeeeb4",
+    "url": "/uk-perfumes/static/media/water2.602bc0e5.png"
+  },
+  {
+    "revision": "6f36fdf6819d3d6cf88f8be56163f90d",
+    "url": "/uk-perfumes/static/media/wood-fruity.6f36fdf6.png"
+  },
+  {
+    "revision": "35819e98d1bccc58315998629c1572c8",
+    "url": "/uk-perfumes/static/media/wood-fruity1.35819e98.png"
+  },
+  {
+    "revision": "10c588a5019a28e77b2886dabc77e7f2",
+    "url": "/uk-perfumes/static/media/woodsy.10c588a5.png"
+  },
+  {
+    "revision": "b9e9cefdecc33da772f0761980d9dc06",
+    "url": "/uk-perfumes/static/media/woodsy1.b9e9cefd.png"
+  },
+  {
+    "revision": "003c191eb20ac46297d02144142db5ef",
+    "url": "/uk-perfumes/static/media/woody.003c191e.png"
+  },
+  {
+    "revision": "1310c37a6870155dcc8774c149859331",
+    "url": "/uk-perfumes/static/media/woody1.1310c37a.png"
+  },
+  {
+    "revision": "db049f5abb06411c9cc6408d281e8f2d",
+    "url": "/uk-perfumes/static/media/ylang-1.db049f5a.png"
+  },
+  {
+    "revision": "7c315d687598012a4ac6aede22b943c3",
+    "url": "/uk-perfumes/static/media/ylang1.7c315d68.png"
+  }
+]);
