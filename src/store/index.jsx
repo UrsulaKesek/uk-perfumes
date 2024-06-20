19f829bd80a6842b1d48264f60e9b7e27a3390df
Ursula Kesek <ursky@blueyonder.co.uk>
2024-06-20 16:06:52 +0100

    edited splice parameters

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -29,7 +29,7 @@ const Store = () => {
     if (index >= 0) {
       setCart((cart) => {
         const cartCopy = [...cart];
-        cartCopy.splice(item.length, item.count);
+        cartCopy.splice(item.length, item);
         return cartCopy;
       });
     }
