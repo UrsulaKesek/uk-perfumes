5f6b552583ea4901d71a4f27f361f4fe2062e47a
Ursula Kesek <ursky@blueyonder.co.uk>
2024-06-23 10:42:15 +0100

    edited splice to slice

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -41,7 +41,7 @@ const Store = () => {
     if (index >= 0) {
       setCart((cart) => {
         const cartCopy = [...cart];
-        cartCopy.splice(index,item.count);
+        cartCopy.slice(index,item.count);
         console.log(item.count);
         console.log(cartCopy);
         return cartCopy;
